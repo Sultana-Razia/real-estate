6d62bad0b0070a9da725cea2cc0c276f4b9bd42f
Sultana Razia <sultanarazia483@gmail.com>
2024-09-08 18:03:19 +0600

    Create Auth Context

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,6 +4,16 @@ import Footer from "../../Shared/Footer/Footer";
 
 
 const Login = () => {
+
+    const handleLogin = e => {
+        e.preventDefault();
+        // const email = e.target.email.value;
+        console.log(e.currentTarget);
+        const form = new FormData(e.currentTarget);
+        console.log(form.get('email'));
+        console.log(form.get('password'));
+    }
+
     return (
         <div>
             <Navbar></Navbar>
@@ -11,26 +21,23 @@ const Login = () => {
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="text-center lg:text-left">
                         <h1 className="text-5xl font-bold">Login now!</h1>
-                        <p className="py-6">
-                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                            quasi. In deleniti eaque aut repudiandae et a id nisi.
-                        </p>
+                        <p className="py-6">Welcome back! Please enter your credentials to log into your account.</p>
                     </div>
                     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                        <form className="card-body">
+                        <form onSubmit={handleLogin} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
+                                <input type="email" name="email" placeholder="email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <input type="password" name="password" placeholder="password" className="input input-bordered" required />
                                 <label className="label">
-                                    <p>New to this website please <Link to='/register' className="text-blue-500 font-bold">Register</Link></p>
+                                    <p>Do not have an account please <Link to='/register' className="text-blue-500 font-bold">Register</Link></p>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -7,15 +7,30 @@ import Footer from "../../Shared/Footer/Footer";
 
 const Register = () => {
 
-    const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors },
-    } = useForm()
-    const onSubmit = (data) => console.log(data)
+    const handleRegister = e => {
+        e.preventDefault();
+        // const email = e.target.email.value;
+        console.log(e.currentTarget);
+        const form = new FormData(e.currentTarget);
+        const name = form.get('name');
+        const photo = form.get('photo');
+        const email = form.get('email');
+        const password = form.get('password');
+        console.log(name);
+        console.log(photo);
+        console.log(email);
+        console.log(password);
+    }
 
-    console.log(watch("example"))
+    // const {
+    //     register,
+    //     handleSubmit,
+    //     watch,
+    //     formState: { errors },
+    // } = useForm()
+    // const onSubmit = (data) => console.log(data)
+
+    // console.log(watch("example"))
 
     return (
         <div>
@@ -24,38 +39,35 @@ const Register = () => {
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="text-center lg:text-left">
                         <h1 className="text-5xl font-bold">Register now!</h1>
-                        <p className="py-6">
-                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                            quasi. In deleniti eaque aut repudiandae et a id nisi.
-                        </p>
+                        <p className="py-6">Create your account to access exclusive features and personalized content.</p>
                     </div>
                     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                        <form className="card-body">
+                        <form onSubmit={handleRegister} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Your Name</span>
                                 </label>
-                                <input type="text" placeholder="Your Name" className="input input-bordered" required />
+                                <input type="text" name="name" placeholder="Your Name" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="Email" className="input input-bordered" required />
+                                <input type="email" name="email" placeholder="Email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">PhotoURL</span>
                                 </label>
-                                <input type="text" placeholder="PhotoURL" className="input input-bordered" required />
+                                <input type="text" name="photo" placeholder="PhotoURL" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="Password" className="input input-bordered" required />
+                                <input type="password" name="password" placeholder="Password" className="input input-bordered" required />
                                 <label className="label">
-                                    <p>Already have an account please <Link to='/login' className="text-blue-500 font-bold">Login</Link></p>
+                                    <p>Already have an account? please <Link to='/login' className="text-blue-500 font-bold">Login</Link></p>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
